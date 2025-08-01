c0c68f683f662ded8f543b8478962a3cafcb8494
umerabelim <umerabelim72@gmail.com>
2025-08-01 20:29:07 +0530

    Add files via upload

---
+++ inc.java
@@ -0,0 +1,11 @@
+public class inc{
+public static void main (String[] args){
+int a=10;
+System.out.println("a++:"+(a++));
+System.out.println("a:"+(a));
+System.out.println("++a:"+(++a));
+System.out.println("a--:"+(a--));
+System.out.println("a:"+(a));
+System.out.println("--a"+(--a));
+}
+}
